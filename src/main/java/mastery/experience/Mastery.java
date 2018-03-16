dcdfeb2b2b523b1bd0ddc5ba16c40b04da05206b
Granis <arne.maschmann@hotmail.com>
2018-03-16 21:31:58 +0100

    Mastery persistence first steps

---
+++ src/main/java/mastery/experience/Mastery.java
@@ -0,0 +1,14 @@
+package mastery.experience;
+
+/**
+ * Created by Granis on 16/03/2018.
+ */
+public class Mastery {
+
+    private int miningMastery;
+
+    public Mastery(int miningMastery) {
+        this.miningMastery = miningMastery;
+    }
+
+}
--- src/main/java/mastery/experience/PlayerExperience.java
+++ src/main/java/mastery/experience/PlayerExperience.java
@@ -1,8 +1,24 @@
 package mastery.experience;
 
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+
+import javax.annotation.Nullable;
+
 /**
  * Created by Granis on 15/03/2018.
  */
-public class PlayerExperience {
+public class PlayerExperience implements Capability.IStorage<Mastery> {
 
+    @Nullable
+    @Override
+    public NBTBase writeNBT(Capability<Mastery> capability, Mastery mastery, EnumFacing enumFacing) {
+        return null;
+    }
+
+    @Override
+    public void readNBT(Capability<Mastery> capability, Mastery mastery, EnumFacing enumFacing, NBTBase nbtBase) {
+//        nbtBase.
+    }
 }

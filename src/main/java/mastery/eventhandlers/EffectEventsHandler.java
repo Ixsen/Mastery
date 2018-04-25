9f29a10ff6789259c852be36f529532921617ed2
Granis <arne.maschmann@hotmail.com>
2018-04-25 23:37:01 +0200

    nerfed a bit

--- src/main/java/mastery/eventhandlers/EffectEventsHandler.java
+++ src/main/java/mastery/eventhandlers/EffectEventsHandler.java
@@ -2,6 +2,7 @@ package mastery.eventhandlers;
 
 import mastery.experience.IMastery;
 import mastery.experience.MasteryProvider;
+import mastery.experience.skillclasses.CraftingMastery;
 import mastery.experience.skillclasses.MASTERY_SPEC;
 import mastery.experience.skillclasses.MiningMastery;
 import net.minecraft.util.text.TextComponentString;
@@ -12,6 +13,8 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Random;
 
 public class EffectEventsHandler {
 
@@ -52,6 +55,15 @@ public class EffectEventsHandler {
         MiningMastery miningMastery = (MiningMastery) mastery.getMasteries().get(MASTERY_SPEC.MINING);
         float newSpeed = miningMastery.getMiningSpeed(breakingSpeed.getOriginalSpeed());
         breakingSpeed.setNewSpeed(newSpeed);
+    }
+
+    @SubscribeEvent
+    public void onItemCrafted(net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemCraftedEvent event) {
+        IMastery mastery = event.player.getCapability(MasteryProvider.MASTERY_CAPABILITY, null);
+        CraftingMastery craftingMastery = (CraftingMastery) mastery.getMasteries().get(MASTERY_SPEC.CRAFTING);
+        Random random = new Random();
+
+        event.crafting.setCount(event.crafting.getCount() + (random.nextInt(100) + craftingMastery.getLevel()) / 100);
     }
 
 }

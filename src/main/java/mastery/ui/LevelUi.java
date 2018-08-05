c225eb2042de0252ff7b9b2bcd3dcccbc694685a
Enis Belli <Enis.Belli@LMIS.de>
2018-08-05 13:14:57 +0200

    refactoring of levelui

--- src/main/java/mastery/ui/LevelUi.java
+++ src/main/java/mastery/ui/LevelUi.java
@@ -1,7 +1,5 @@
 package mastery.ui;
 
-import mastery.experience.IMastery;
-import mastery.experience.skillclasses.MASTERY_SPEC;
 import mastery.util.MasteryUtils;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiButton;
@@ -11,8 +9,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class LevelUi extends GuiScreen {
-    public static final int WIDTH = 150;
-    public static final int HEIGHT = 150;
+    private static final int WIDTH = 150;
+    private static final int HEIGHT = 150;
 
     public LevelUi() {
         super();
@@ -27,15 +25,14 @@ public class LevelUi extends GuiScreen {
     @Override
     public void initGui() {
         EntityPlayerSP player = this.mc.player;
-        IMastery mastery = MasteryUtils.getMasteries(player);
-        GuiButton alchemy = new GuiButton(0, 100, 21, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.ALCHEMY).toString());
-        GuiButton athletics = new GuiButton(0, 100, 42, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.ATHLETICS).toString());
-        GuiButton combat = new GuiButton(0, 100, 63, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.COMBAT).toString());
-        GuiButton crafting = new GuiButton(0, 100, 84, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.CRAFTING).toString());
-        GuiButton farming = new GuiButton(0, 100, 105, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.FARMING).toString());
-        GuiButton husbandry = new GuiButton(0, 100, 126, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.HUSBANDRY).toString());
-        GuiButton mining = new GuiButton(0, 100, 147, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.MINING).toString());
-        GuiButton survival = new GuiButton(0, 100, 168, 300, 20, mastery.getMasteries().get(MASTERY_SPEC.SURVIVAL).toString());
+        GuiButton alchemy = new GuiButton(0, 100, 21, 300, 20, MasteryUtils.getAlchemyMastery(player).toString());
+        GuiButton athletics = new GuiButton(0, 100, 42, 300, 20, MasteryUtils.getAthleticsMastery(player).toString());
+        GuiButton combat = new GuiButton(0, 100, 63, 300, 20, MasteryUtils.getCombatMastery(player).toString());
+        GuiButton crafting = new GuiButton(0, 100, 84, 300, 20, MasteryUtils.getCraftingMastery(player).toString());
+        GuiButton farming = new GuiButton(0, 100, 105, 300, 20, MasteryUtils.getFarmingMastery(player).toString());
+        GuiButton husbandry = new GuiButton(0, 100, 126, 300, 20, MasteryUtils.getHusbandryMastery(player).toString());
+        GuiButton mining = new GuiButton(0, 100, 147, 300, 20, MasteryUtils.getMiningMastery(player).toString());
+        GuiButton survival = new GuiButton(0, 100, 168, 300, 20, MasteryUtils.getSurvivalMastery(player).toString());
         this.buttonList.add(alchemy);
         this.buttonList.add(athletics);
         this.buttonList.add(combat);
@@ -44,20 +41,5 @@ public class LevelUi extends GuiScreen {
         this.buttonList.add(husbandry);
         this.buttonList.add(mining);
         this.buttonList.add(survival);
-
-
-        //GUILabel(FontRenderer, ID, X, Y, WIDTH, HEIGHT, TEXT_COLOR)
-//        GuiLabel level = new GuiLabel(fontRenderer, 100, 2, 2, 200, 50, 4210752);
-//        level.addLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-//        GuiLabel level2 = new GuiLabel(fontRenderer, 100, 2, 25, 200, 50, 4210752);
-//        level.addLine("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
-//        GuiLabel level3 = new GuiLabel(fontRenderer, 100, 2, 50, 200, 50, 4210752);
-//        level.addLine("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
-//        GuiLabel level4 = new GuiLabel(fontRenderer, 100, 2, 75, 200, 50, 1000);
-//        level.addLine("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
-//        labelList.add(level);
-//        labelList.add(level2);
-//        labelList.add(level3);
-//        labelList.add(level4);
     }
 }

8ff79104c01d9bf5703128ca482bd4eb49b2cab7
Ixsen <ixsen42@gmail.com>
2018-08-08 22:23:40 +0200

    Grundstruktur angepasst und erste Klassen umgestellt

--- src/main/java/mastery/eventhandlers/ExperienceEventsHandler.java
+++ src/main/java/mastery/eventhandlers/ExperienceEventsHandler.java
@@ -1,49 +1,10 @@
 package mastery.eventhandlers;
 
 import net.minecraftforge.event.entity.living.AnimalTameEvent;
-import net.minecraftforge.event.entity.player.BonemealEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.entity.player.UseHoeEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ExperienceEventsHandler {
-
-    public ExperienceEventsHandler() {
-    }
-
-    /**
-     * TODO FARMING
-     *
-     * @param harvestCheck
-     *            --
-     */
-    @SubscribeEvent
-    public void harvestCrop(PlayerEvent.HarvestCheck harvestCheck) {
-
-    }
-
-    /**
-     * TODO FARMING
-     *
-     * @param bonemealEvent
-     *            --
-     */
-    @SubscribeEvent
-    public void boneEvent(BonemealEvent bonemealEvent) {
-
-    }
-
-    /**
-     * TODO FARMING
-     *
-     * @param useHoeEvent
-     *            --
-     */
-    @SubscribeEvent
-    public void hoeing(UseHoeEvent useHoeEvent) {
-
-    }
 
     /**
      * TODO HUSBANDRY
@@ -65,8 +26,4 @@ public class ExperienceEventsHandler {
     public void feed(PlayerInteractEvent.EntityInteractSpecific interactEvent) {
 
     }
-
-    /*
-     * TODO TNT MINING
-     */
 }
--- src/main/java/mastery/eventhandlers/mining/MiningExperience.java
+++ src/main/java/mastery/eventhandlers/mining/MiningExperience.java
@@ -27,4 +27,7 @@ public class MiningExperience {
         }
     }
 
+    /*
+     * TNT MINING
+     */
 }

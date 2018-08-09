700a7386df554da43578afa9367d7bc785ce2cca
Ixsen <ixsen42@gmail.com>
2018-08-09 18:25:30 +0200

    Husbandry stuff changed, pre update commit

--- src/main/java/mastery/capability/skillclasses/HusbandryMastery.java
+++ src/main/java/mastery/capability/skillclasses/HusbandryMastery.java
@@ -2,16 +2,6 @@ package mastery.capability.skillclasses;
 
 public class HusbandryMastery extends MasteryClass {
 
-    public enum EXP_TYPE {
-        ENTITY_TAMED(10), ANIMAL_HARVEST(4), ANIMAL_FED(1), ANIMAL_CHILD_SPAWN(1);
-
-        public int value = 1;
-
-        EXP_TYPE(int value) {
-            this.value = value;
-        }
-    }
-
     public HusbandryMastery() {
         this.name = "Husbandry";
     }
@@ -21,7 +11,4 @@ public class HusbandryMastery extends MasteryClass {
         return MasterySpec.HUSBANDRY;
     }
 
-    public void increaseExperience(EXP_TYPE type) {
-        this.increaseExperience(type.value);
-    }
 }
--- src/main/java/mastery/eventhandlers/husbandry/HusbandryExperience.java
+++ src/main/java/mastery/eventhandlers/husbandry/HusbandryExperience.java
@@ -1,4 +1,33 @@
 package mastery.eventhandlers.husbandry;
 
-public class HusbandryExperience {
+import mastery.capability.skillclasses.MasterySpec;
+import mastery.eventhandlers.AbstractExperienceHandler;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.living.AnimalTameEvent;
+import net.minecraftforge.event.entity.living.BabyEntitySpawnEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import static mastery.eventhandlers.ExperienceDictionary.HUSBANDRY_ANIMAL_CHILD_SPAWN;
+import static mastery.eventhandlers.ExperienceDictionary.HUSBANDRY_ENTITY_TAMED;
+
+public class HusbandryExperience extends AbstractExperienceHandler {
+
+    private ItemStack mainHand;
+    private ItemStack offHand;
+
+    private boolean secondTrigger = false;
+
+    public HusbandryExperience() {
+        this.spec = MasterySpec.HUSBANDRY;
+    }
+
+    @SubscribeEvent
+    public void animalTame(AnimalTameEvent tameEvent) {
+        this.addExperience(tameEvent.getTamer(), HUSBANDRY_ENTITY_TAMED);
+    }
+
+    @SubscribeEvent
+    public void spawnBaby(BabyEntitySpawnEvent babyEntitySpawnEvent) {
+        this.addExperience(babyEntitySpawnEvent.getCausedByPlayer(), HUSBANDRY_ANIMAL_CHILD_SPAWN);
+    }
 }

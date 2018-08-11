730f93cc288a70e1e1e536a785a36179432d7e2f
Ixsen <ixsen42@gmail.com>
2018-08-11 21:49:21 +0200

    Fixed exp for mining and added tnt-mining

--- src/main/java/mastery/eventhandlers/mining/MiningExperience.java
+++ src/main/java/mastery/eventhandlers/mining/MiningExperience.java
@@ -1,26 +1,31 @@
 package mastery.eventhandlers.mining;
 
-import mastery.capability.skillclasses.MiningMastery;
+import mastery.capability.skillclasses.MasterySpec;
 import mastery.eventhandlers.AbstractExperienceHandler;
-import mastery.util.MasteryUtils;
-import mastery.util.NetworkUtils;
-import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class MiningExperience extends AbstractExperienceHandler {
 
+    public MiningExperience() {
+        this.spec = MasterySpec.MINING;
+    }
+
     @SubscribeEvent
     public void breakBlock(BlockEvent.BreakEvent breakEvent) {
         if (!breakEvent.getPlayer().getEntityWorld().isRemote) {
-            EntityPlayerMP player = (EntityPlayerMP) breakEvent.getPlayer();
-            MiningMastery miningMastery = MasteryUtils.getMiningMastery(player);
-            miningMastery.increaseExperience();
-            NetworkUtils.sendExpToPlayer(miningMastery, player);
+            this.addExperience(breakEvent.getPlayer(), MiningUtils.expAmountForBlock(breakEvent.getState().getBlock()));
         }
     }
 
-    /*
-     * TNT MINING
-     */
+    @SubscribeEvent
+    public void explosion(ExplosionEvent.Detonate event) {
+        if (event.getExplosion().getExplosivePlacedBy() instanceof EntityPlayer
+                && !event.getExplosion().getExplosivePlacedBy().getEntityWorld().isRemote) {
+            this.addExperience(event.getExplosion().getExplosivePlacedBy(),
+                    MiningUtils.expForExplosion(event.getExplosion()));
+        }
+    }
 }
---
+++ src/main/java/mastery/eventhandlers/mining/MiningUtils.java
@@ -0,0 +1,58 @@
+package mastery.eventhandlers.mining;
+
+import static mastery.eventhandlers.ExperienceDictionary.MINING_BLOCK;
+import static mastery.eventhandlers.ExperienceDictionary.MINING_ORE;
+
+import java.util.HashMap;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockOre;
+import net.minecraft.block.IGrowable;
+import net.minecraft.client.Minecraft;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
+import net.minecraftforge.common.IPlantable;
+
+class MiningUtils {
+
+    private static HashMap<Block, Integer> expMap = new HashMap<>();
+
+    static {
+        expMap.put(Blocks.COAL_ORE, 1);
+
+        expMap.put(Blocks.IRON_ORE, 2);
+
+        expMap.put(Blocks.REDSTONE_ORE, 3);
+        expMap.put(Blocks.LIT_REDSTONE_ORE, 3);
+        expMap.put(Blocks.LAPIS_ORE, 3);
+        expMap.put(Blocks.QUARTZ_ORE, 3);
+
+        expMap.put(Blocks.GOLD_ORE, 4);
+
+        expMap.put(Blocks.DIAMOND_ORE, 5);
+        expMap.put(Blocks.EMERALD_ORE, 5);
+
+    }
+
+    private static Block getBlockFromPosition(BlockPos pos) {
+        return Minecraft.getMinecraft().world.getBlockState(pos).getBlock();
+    }
+
+    static int expAmountForBlock(Block block) {
+        if (block instanceof BlockOre || block == Blocks.REDSTONE_ORE) {
+            Integer expAmount = expMap.get(block) != null ? expMap.get(block)
+                    : (1 + block.getHarvestLevel(block.getDefaultState()));
+            return MINING_ORE.getValue() * expAmount;
+        } else if (!(block instanceof IGrowable) && !(block instanceof IPlantable) && !(block == Blocks.AIR)
+                && !(block == Blocks.BEDROCK)) {
+            return MINING_BLOCK.getValue();
+        }
+        return 0;
+    }
+
+    static int expForExplosion(Explosion explosion) {
+        return explosion.getAffectedBlockPositions().stream()
+                .mapToInt(blockPos -> expAmountForBlock(getBlockFromPosition(blockPos))).sum();
+    }
+}

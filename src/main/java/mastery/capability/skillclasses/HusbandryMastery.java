a57dda6b503503abe706d354cb23077c0c316257
Ixsen <ixsen42@gmail.com>
2018-08-18 20:40:32 +0200

    Added effects for husbandry

--- src/main/java/mastery/capability/skillclasses/HusbandryMastery.java
+++ src/main/java/mastery/capability/skillclasses/HusbandryMastery.java
@@ -2,6 +2,10 @@ package mastery.capability.skillclasses;
 
 public class HusbandryMastery extends MasteryClass {
 
+    public static final double TWIN_CHANCE = 0.01;
+    public static final float MIN_GROW_MULTIPLIER = 0.1f;
+    public static final double BASE_TAME_CHANCE = 0.1f;
+
     public HusbandryMastery() {
         this.name = "Husbandry";
     }
@@ -9,6 +13,30 @@ public class HusbandryMastery extends MasteryClass {
     @Override
     public MasterySpec getSkillClass() {
         return MasterySpec.HUSBANDRY;
+    }
+
+    public boolean willSpawnTwins() {
+        return Math.random() <= Math.min(TWIN_CHANCE * this.getLevel(), 1);
+    }
+
+    public float getGrowingMultiplier() {
+        return Math.min(1f / this.getLevel(), MIN_GROW_MULTIPLIER);
+    }
+
+    public boolean willBeTamed() {
+        return Math.random() < Math.min(0.7, BASE_TAME_CHANCE * this.getLevel());
+    }
+
+    public double getPetDamageModifier() {
+        return this.getLevel();
+    }
+
+    public double getPetMovementModifier() {
+        return this.getLevel();
+    }
+
+    public double getPetHealthModifier() {
+        return this.getLevel();
     }
 
 }
